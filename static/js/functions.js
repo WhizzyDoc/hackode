8ecd85c90d42f22e20ea66debb0d0331723d0a17
WhizzyDoc <hassanridwan2536@gmail.com>
2024-03-20 14:55:51 +0100

    a

--- static/js/functions.js
+++ static/js/functions.js
@@ -1,6 +1,6 @@
-const base_image_url = `http://127.0.0.1:8000`;
+//const base_image_url = `http://127.0.0.1:8000`;
 const base_image_url = `https://riganapi.pythonanywhere.com`;
-//const base_url = `${base_image_url}/api/v3/`;
+const base_url = `${base_image_url}/api/v3/`;
 
 
 /* Navigation bar */
